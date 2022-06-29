11e9bf6bc980db26b5878df60a373944ab1aa803
Tiago <tiago.olive.cam@gmail.com>
2022-06-29 14:04:04 -0300

    UseDebounce adicionado

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -2,29 +2,33 @@ import { useEffect, useMemo } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
 import { FerramentasDaListagem } from '../../shared/components'
+import { useDebounce } from '../../shared/hooks'
 import { LayoutBaseDePagina } from '../../shared/layouts'
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService'
 
 
 
-
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
+  const { debounce } = useDebounce(1000)
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || ''    
   }, [searchParams])
 
   useEffect(() => {
+
+    debounce(() => {
+      PessoasService.getAll(1, busca)
+        .then((result) => {
+          if(result instanceof Error) {
+            alert(result.message)
+          } else {
+            console.log(result)
+          }
+        })
+    })
     
-    PessoasService.getAll(1, busca)
-      .then((result) => {
-        if(result instanceof Error) {
-          alert(result.message)
-        } else {
-          console.log(result)
-        }
-      })
 
   }, [busca])
 

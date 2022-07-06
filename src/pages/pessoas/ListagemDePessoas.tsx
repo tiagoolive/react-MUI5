46aab1aad31f21bd74551290a445c75403a0d9db
Tiago <tiago.olive.cam@gmail.com>
2022-07-06 15:27:17 -0300

    Carregamento da lista e lista vazia adicionados

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,8 +1,9 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material'
 import { useEffect, useMemo, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
 import { FerramentasDaListagem } from '../../shared/components'
+import { Environment } from '../../shared/environment'
 import { useDebounce } from '../../shared/hooks'
 import { LayoutBaseDePagina } from '../../shared/layouts'
 import { PessoasService, IListagemPessoa } from '../../shared/services/api/pessoas/PessoasService'
@@ -62,16 +63,28 @@ export const ListagemDePessoas: React.FC = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-
-            ({rows.map(row => (
+            {rows.map(row => (
               <TableRow key={row.id}>
                 <TableCell>Ações</TableCell>
                 <TableCell>{row.nomeCompleto}</TableCell>
                 <TableCell>{row.email}</TableCell>
               </TableRow>
-            ))})
-
+            ))}
           </TableBody>
+
+          {totalCount === 0 && !isLoading && (
+            <caption>{Environment.LISTAGEM_VAZIA}</caption>
+          )}    
+
+          <TableFooter>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <LinearProgress variant='indeterminate' />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableFooter>
         </Table>
       </TableContainer>
     </LayoutBaseDePagina>

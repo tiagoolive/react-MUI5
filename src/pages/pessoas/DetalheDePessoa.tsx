37393ad2cfea4a64c212f5a6002fcd447566e7d3
Tiago <tiago.olive.cam@gmail.com>
2022-07-07 15:14:51 -0300

    Botão de deletar, voltar e loading configurados

--- src/pages/pessoas/DetalheDePessoa.tsx
+++ src/pages/pessoas/DetalheDePessoa.tsx
@@ -1,24 +1,59 @@
+import { LinearProgress } from '@mui/material'
+import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 import { FerramentasDeDetalhe } from '../../shared/components'
 import { LayoutBaseDePagina } from './../../shared/layouts/LayoutBaseDePagina'
+import { PessoasService } from './../../shared/services/api/pessoas/PessoasService'
 
 
 export const DetalheDePessoa: React.FC = () => {
   const { id = 'nova' } = useParams<'id'>()
   const navegate = useNavigate()
 
+  const [isLoading, setIsLoading] = useState(false)
+  const [nome, setNome] = useState('')
+
+  useEffect(() => {
+    if (id !== 'nova') {
+      setIsLoading(true)
+
+      PessoasService.getById(Number(id))
+        .then((result) => {
+          setIsLoading(false)
+
+          if (result instanceof Error) {
+            alert(result.message)
+            navegate('/pessoas')
+          } else {
+            setNome(result.nomeCompleto)
+            console.log(result)
+          }
+        })
+    }
+  }, [id])
+
   const handleSave = () => {
     console.log('Save')
   }
 
-  const handleDelete = () => {
-    console.log('Save')
+  const handleDelete = (id: number) => {
+    if(confirm('Realmente deseja apagar?')){
+      PessoasService.deleteById(id)
+        .then(result => {
+          if(result instanceof Error){
+            alert(result.message)
+          } else {
+            alert('Registro apagado com sucesso')
+            navegate('/pessoas')
+          }
+        })
+    }
   }
 
   return (
-    <LayoutBaseDePagina 
-      titulo='Detalhe de pessoa'
+    <LayoutBaseDePagina
+      titulo={id === 'nova' ? 'Nova pessoa' : nome}
       barraDeFerramentas={
         <FerramentasDeDetalhe
           textoBotaoNovo='Nova'
@@ -28,12 +63,17 @@ export const DetalheDePessoa: React.FC = () => {
 
           aoClicarEmSalvar={handleSave}
           aoClicarEmSalvarEFechar={handleSave}
-          aoClicarEmApagar={handleDelete}
+          aoClicarEmApagar={() => handleDelete(Number(id))}
           aoClicarEmVoltar={() => navegate('/pessoas')}
           aoClicarEmNovo={() => navegate('/pessoas/detalhe/nova')}
         />
       }
     >
+
+      {isLoading && (
+        <LinearProgress variant='indeterminate' />
+      )}
+
       <p>DetalheDePessoa {id}</p>
 
     </LayoutBaseDePagina>
